afc4527956f06aa46b7c8cd2d066d423bcc69b40
Daniel Verdu <iamadeveloper.es@gmail.com>
2023-09-05 14:11:35 +0200

    Comienzo: componentes app-image, app-picture

---
+++ src/components/atoms/app-image/demo/index.vue
@@ -0,0 +1,21 @@
+<script lang="ts">
+
+import AppImage from '../index.vue'
+
+export default {
+    name: 'app-image-demo',
+    components: {AppImage},
+    setup(){
+        const src = 'src/assets/images/vue.jpg' 
+        return{
+            src
+        }
+    }
+}
+</script>
+
+<template lang="pug">
+app-image(
+:src="src", 
+alt="Imagen de Vue 3")
+</template>
---
+++ src/components/atoms/app-image/index.vue
@@ -0,0 +1,31 @@
+<script lang="ts">
+
+export default {
+    name: 'app-image',
+    props: {
+        src: {
+            type: String,
+            required: true
+        },
+        alt: {
+            type: String
+        }
+    },
+    setup(){
+
+        return{
+            
+        }
+    }
+}
+</script>
+
+<template lang="pug">
+img.app-image(
+:src="src", 
+:alt="alt")
+</template>
+
+<style lang="scss">
+@import 'index';
+</style>
