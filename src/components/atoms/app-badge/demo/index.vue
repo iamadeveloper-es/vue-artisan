6ab1c938458c8cb5db91cad2c0a1a1a7f4f66b67
Daniel Verdu <iamadeveloper.es@gmail.com>
2024-12-01 01:34:43 +0100

    Comienzo app-badge

---
+++ src/components/atoms/app-badge/demo/index.vue
@@ -0,0 +1,37 @@
+<script lang="ts">
+export default {
+  name: 'app-badge-demo'
+};
+</script>
+<script setup lang="ts">
+import { ref } from 'vue';
+import AppBadge from '../index.vue';
+import AppIcon from '../../app-icon/index.vue';
+import AppButton from '../../../molecules/app-button/index.vue';
+
+const badgeCount = ref(98);
+
+const increment = () => {
+  badgeCount.value++;
+};
+
+</script>
+
+<template lang="pug">
+app-badge(:count="badgeCount")
+  template(#content)
+    app-icon(
+      :icon="['fa-solid', 'fa-image']",
+      size="25"
+      variant="primary")
+div
+  app-button(
+    @clicked="increment",
+    text="incrementar", )
+</template>
+
+<style lang="scss">
+.app-button{
+  margin-top: 2em;
+}
+</style>
