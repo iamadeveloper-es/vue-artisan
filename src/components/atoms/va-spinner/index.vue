332ff875dd4d1fdb27dce7795731866a0b16f7c5
daniel verdu <iamadeveloper.es@gmail.com>
2025-01-08 00:29:53 +0100

    Refactor va-spinner

---
+++ src/components/atoms/va-spinner/index.vue
@@ -0,0 +1,26 @@
+<script lang="ts">
+export default {
+  name: 'va-spinner'
+};
+</script>
+<script setup lang="ts">
+
+defineProps({
+  show: {
+    type: Boolean,
+    default: true
+  }
+});
+</script>
+
+<template>
+<div class="va-spinner" v-if="show">
+  <svg class="spinner" viewBox="0 0 50 50">
+    <circle class="path" cx="25" cy="25" r="20" fill="none" stroke-width="5"></circle>
+  </svg>
+</div>
+</template>
+
+<style lang="scss">
+@import 'index';
+</style>
