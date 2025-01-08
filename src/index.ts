a23b2ff04a961a704674f1be594a56c60fcebef3
daniel verdu <iamadeveloper.es@gmail.com>
2025-01-08 09:45:11 +0100

    Actualizo exportaciones de librería

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,20 @@
+//Atoms:
 import VAAvatar from './components/atoms/va-avatar/index.vue';
+import VABadge from './components/atoms/va-badge/index.vue';
+import VACircularProgress from './components/atoms/va-circular-progress/index.vue';
+import VAIcon from './components/atoms/va-icon/index.vue';
+import VAIconSvg from './components/atoms/va-icon-svg/index.vue';
+
+//Molecules
 import VAInputField from './components/molecules/va-input-field/index.vue';
 import VAButton from './components/molecules/va-button/index.vue';
 
 export{
   VAAvatar,
+  VABadge,
+  VACircularProgress,
+  VAIcon,
+  VAIconSvg,
   VAInputField,
   VAButton
 };
